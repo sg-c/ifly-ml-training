d62431608455e46ffdad1b060e6f4da4be70d542
Saiguang Che <saiguang@alluxio.com>
2023-06-09 17:16:44 +0800

    try sth

--- tool/remote-execute.sh
+++ tool/remote-execute.sh
@@ -4,5 +4,5 @@ CMD="cd ifly-ml-training; git pull; ./script/command.sh"
 
 # Set up the nodes
 ssh fuse-test-1 "$CMD"
-# ssh fuse-test-2 "$CMD"
-# ssh fuse-test-3 "$CMD"
+ssh fuse-test-2 "$CMD"
+ssh fuse-test-3 "$CMD"
