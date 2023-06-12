ae56b60572f0f9d8ce7cba08add671a7626d5dae
Saiguang Che <saiguang@alluxio.com>
2023-06-12 11:11:07 +0800

    try sth

--- script/prepare.sh
+++ script/prepare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # install java11 and other softwares
-sudo yum install -y java-11-amazon-corretto git tmux
+sudo yum install -y java-11-amazon-corretto tmux
 
 # mount ebs for data
 # format the disk
@@ -10,6 +10,3 @@ sudo mkfs -t ext4 /dev/sdb
 sudo mkdir /mnt/data
 # mount the disk
 sudo mount /dev/sdb /mnt/data
-
-# clone repo
-[ ! -d ifly-ml-training ] && git clone https://github.com/sg-c/ifly-ml-training.git
--- tool/prepare-nodes.sh
+++ tool/prepare-nodes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Read the AWS credentials for the default profile from the file
 AWS_ACCESS_KEY_ID=$(awk -F "=" '/^\[default\]$/{flag=1} flag && /aws_access_key_id/{print $2; flag=0}' ~/.aws/credentials | tr -d ' ')
 AWS_SECRET_ACCESS_KEY=$(awk -F "=" '/^\[default\]$/{flag=1} flag && /aws_secret_access_key/{print $2; flag=0}' ~/.aws/credentials | tr -d ' ')
@@ -11,10 +13,10 @@ SET_REGION="aws configure set region us-east-1"
 SET_FORMAT="aws configure set output json"
 SET_UP_AWS="$SET_KEY_ID && $SET_SECRET && $SET_REGION && $SET_FORMAT"
 
-# Execute the prepare.sh
-PREPARE=""
+# clone repo
+CLONE_REPO="sudo yum install -y git; [ ! -d ifly-ml-training ] && git clone https://github.com/sg-c/ifly-ml-training.git; "
 
 # Set up the nodes
-ssh fuse-test-1 "$SET_UP_AWS;"
-ssh fuse-test-2 "$SET_UP_AWS;"
-ssh fuse-test-3 "$SET_UP_AWS;"
+ssh fuse-test-1 "$SET_UP_AWS; $CLONE_REPO"
+ssh fuse-test-2 "$SET_UP_AWS; $CLONE_REPO"
+ssh fuse-test-3 "$SET_UP_AWS; $CLONE_REPO"
