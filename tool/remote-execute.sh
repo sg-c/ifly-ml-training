7bffe34fb77daff7004e1809e99cd8ea3d833cae
Saiguang Che <saiguang@alluxio.com>
2023-06-12 18:18:20 +0800

    try sth

--- tool/remote-execute.sh
+++ tool/remote-execute.sh
@@ -5,15 +5,13 @@ if [ ! -f ./tool/command.sh ]; then
     exit 1
 fi
 
-CMD="cd ifly-ml-training; git pull; ./script/command.sh"
-
 # Define the EC2 instance names
 instances=("fuse-test-1" "fuse-test-2" "fuse-test-3")
 
 # Upload SSH public key to each EC2 instance
 for instance in "${instances[@]}"; do
     echo "Uploading command.sh to ${instance}..."
-    scp ./command.sh ${instance}:~/ifly-ml-training/tool/
+    scp ./tool/command.sh ${instance}:~/ifly-ml-training/tool/
 
     echo "Execute command.sh on ${instance}..."
     ssh ${instance} "~/ifly-ml-training/tool/command.sh"
