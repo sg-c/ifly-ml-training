c5ca94066c8b75427c34b93ec97f1e92a5ee2501
Saiguang Che <saiguang@alluxio.com>
2023-06-12 19:07:28 +0800

    try sth

--- test/large-file.sh
+++ test/large-file.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-dd if=/dev/zero of=/mnt/data/file-200GB.txt bs=1G count=200
+# dd if=/dev/zero of=/mnt/data/file-200GB.txt bs=1G count=200
+
+# download test file from S3
+aws s3 cp s3://alluxio.saiguang.test/large-files/file-200GB.txt /mnt/data/file-200GB.txt
+#aws s3 cp /mnt/data/file-200GB.txt s3://alluxio.saiguang.test/large-files/file-200GB.txt
+
+# copy from local the large file to alluxio
+alluxio fs -Dalluxio.user.file.writetype.default=MUST_CACHE copyFromLocal /mnt/data/file-200GB.txt /file-200GB.txt
