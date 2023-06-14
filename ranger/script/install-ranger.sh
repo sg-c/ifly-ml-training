ffd26c08d11264a26687a3e48af7bf1308618ccd
Saiguang Che <saiguang@alluxio.com>
2023-06-14 16:46:59 +0800

    try sth

--- ranger/script/install-ranger.sh
+++ ranger/script/install-ranger.sh
@@ -5,19 +5,33 @@ set -e
 sudo yum update -y
 
 # Install required dependencies
-sudo yum install -y java-11-amazon-corretto-devel wget tmux
+sudo yum install -y java-11-amazon-corretto-devel git maven wget
 
-# Download Apache Ranger package
-wget -P /tmp https://downloads.apache.org/ranger/2.3.0/apache-ranger-2.3.0.tar.gz
+# Install MySQL Server
+sudo yum install -y mysql-server
 
-# Extract Apache Ranger package
-tar -xf /tmp/apache-ranger-2.3.0.tar.gz -C /tmp
+# Start and enable MySQL service
+sudo systemctl start mysqld
+sudo systemctl enable mysqld
 
-# Move the extracted files to a desired installation directory
-sudo mv /tmp/apache-ranger-2.3.0 /opt/ranger
+# Secure MySQL installation
+sudo mysql_secure_installation
+
+# Clone Apache Ranger repository
+git clone https://github.com/apache/ranger.git
+cd ranger
+
+# Checkout the desired version
+git checkout tags/release-ranger-2.3.0
+
+# Build Apache Ranger
+mvn clean compile package install assembly:assembly -DskipTests=true -Pall
+
+# Extract the Ranger package
+tar -xf target/ranger-2.3.0.tar.gz -C /opt
 
 # Configure Apache Ranger properties
-cd /opt/ranger
+cd /opt/ranger-2.3.0
 sudo cp install.properties ./install.properties.orig
 sudo sed -i 's|^db_root_user=.*$|db_root_user=rangeradmin|' install.properties
 sudo sed -i 's|^db_root_password=.*$|db_root_password=<db_root_password>|' install.properties
@@ -37,4 +51,4 @@ sudo systemctl enable ranger-admin
 sudo systemctl enable ranger-usersync
 # Add any additional service enabling commands as required
 
-echo "Apache Ranger installation and deployment completed successfully."
+echo "Apache Ranger build and installation completed successfully."
